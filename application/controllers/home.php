c2b1bd390a5419534aa005735d80ed97c5bd2dac
Ovidiu Maja <ovi.dan89@gmail.com>
2013-01-02 06:48:14 +0200

    Oana's modifications on old revision.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,39 +5,44 @@ class Home extends CI_Controller {
     function __construct()
     {
         parent::__construct();
-		
-		// Load the necessary stuff...
-		$this->load->helper(array('language', 'url', 'form', 'account/ssl'));
+    
+    // Load the necessary stuff...
+    $this->load->helper(array('language', 'url', 'form', 'account/ssl'));
         $this->load->library(array('account/authentication'));
-		$this->load->model(array('account/account_model'));
-		$this->lang->load(array('general'));
-		$this->load->model('feed_post_model', 'posts');
-		$this->load->model('slider_model', 'slider');
-	}
-	
-	function index()
-	{
-		maintain_ssl();	
-		if ($this->authentication->is_signed_in())
-		{
-			$data['account'] = $this->account_model->get_by_id($this->session->userdata('account_id'));
-		}
-		setCart(false);
+    $this->load->model(array('account/account_model'));
+    $this->lang->load(array('general'));
+    $this->load->model('feed_post_model', 'posts');
+    $this->load->model('slider_model', 'slider');
+  }
+  
+  function index()
+  {
+    maintain_ssl();  
+    if ($this->authentication->is_signed_in())
+    {
+      $data['account'] = $this->account_model->get_by_id($this->session->userdata('account_id'));
+    }
+    setCart(false);
         $data['cart']=getCart();
-       	$data['items']=$this->posts->get_site_posts(MEDIUM_LOAD_ITEMS);
-		$data['submenus']=getSubmenus();
-		$data['slider']=$this->slider->getSliders(MEDIUM_LOAD_ITEMS);
-		$data['gmap']=getPlace("Iulius Mall").getPlace("21 Decembrie","street");
-		$data['page_info']='<div class="welcome">
-				<h2>Welcome</h2>
-				<p>TODO</p>
-			</div>';
-		
-		$this->load->view('header');
-		$this->load->view('home', isset($data) ? $data : NULL);
-		$this->load->view('footer');
-	}
-	
+         $data['items']=$this->posts->get_site_posts(MEDIUM_LOAD_ITEMS);
+    $data['submenus']=getSubmenus();
+    $data['slider']=$this->slider->getSliders(MEDIUM_LOAD_ITEMS);
+    $data['gmap']=getPlace("Iulius Mall").getPlace("21 Decembrie","street");
+    $data['page_info']='<div class="welcome">
+        <h2>Bun venit</h2>
+          <p>Scopul nostru este <b><u><i> un eveniment planificat:</b> </u></i>
+          
+          <li> in detaliu la cateva click-uri distanta</li>
+          <li> modelat dupa dorintele clientului </li>
+          <li> cu raport calitate pret excelent</li>
+          </ul></p>
+      </div>';
+    
+    $this->load->view('header');
+    $this->load->view('home', isset($data) ? $data : NULL);
+    $this->load->view('footer');
+  }
+  
 }
 
 
---
+++ application/logs/log-2013-01-02.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-01-02 00:52:18 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 00:53:23 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 01:40:21 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 01:40:21 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 01:40:25 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 01:41:42 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 01:48:01 --> Severity: Warning  --> Invalid argument supplied for foreach() /var/www/oana/eventplanning/application/models/slider_model.php 58
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): resource/gallery/gal2/info.xml:2: parser error : XML declaration allowed only at the start of the document /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): &lt;?xml version=&quot;1.0&quot; encoding=&quot;ISO-8859-1&quot;?&gt; /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file():      ^ /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): resource/gallery/gal2/info.xml:58: parser error : Opening and ending tag mismatch: item line 46 and description /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file():     &lt;/description&gt; /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file():                   ^ /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): resource/gallery/gal2/info.xml:59: parser error : Opening and ending tag mismatch: gallery line 3 and item /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file():   &lt;/item&gt; /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file():          ^ /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): resource/gallery/gal2/info.xml:62: parser error : Extra content at the end of the document /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): &lt;/gallery&gt; /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:17:47 --> Severity: Warning  --> simplexml_load_file(): ^ /var/www/oana/eventplanning/application/models/gallery_model.php 21
+ERROR - 2013-01-02 04:19:29 --> 404 Page Not Found --> 
+ERROR - 2013-01-02 04:57:14 --> 404 Page Not Found --> 
+ERROR - 2013-01-02 04:58:32 --> 404 Page Not Found --> 
