12b9b77027b0855d04ac776119fbbda8b3ddc3e9
Ovidiu Maja <ovi.dan89@gmail.com>
2013-01-03 15:17:06 +0200

    Modified home controlller to show upcoming public events and galleries .

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,11 +6,12 @@ class Home extends CI_Controller {
         parent::__construct();
 		
 		// Load the necessary stuff...
-		$this->load->helper(array('language', 'url', 'form', 'account/ssl'));
+		$this->load->helper(array('public_events', 'language', 'url', 'form', 'account/ssl'));
         $this->load->library(array('account/authentication'));
 		$this->load->model(array('account/account_model'));
 		$this->lang->load(array('general'));
 		$this->load->model('feed_post_model', 'posts');
+		$this->load->model('gallery_model', 'gallery');
 		$this->load->model('slider_model', 'slider');
 	}
 	
@@ -29,17 +30,10 @@ class Home extends CI_Controller {
        	$data['items']=$this->posts->get_site_posts(MEDIUM_LOAD_ITEMS);
 		$data['submenus']=getSubmenus();
 		$data['slider']=$this->slider->getSliders(MEDIUM_LOAD_ITEMS);
-		$data['gmap']=getPlace("Iulius Mall").getPlace("21 Decembrie","street");
-		$data['page_info']='<div class="welcome">
-        <h2>Bun venit</h2>
-          <p>Scopul nostru este <b><u><i> un eveniment planificat:</b> </u></i>
-          <ul>
-          <li> in detaliu la cateva click-uri distanta</li>
-          <li> modelat dupa dorintele clientului </li>
-          <li> cu raport calitate pret excelent</li>
-          </ul></p>
-      </div>';
-		
+//		$data['gmap']=getPlace("Iulius Mall").getPlace("21 Decembrie","street");
+		$data['page_info']='<div class="welcome">'. lang('welcome_message') . '</div><br/><h2>'. lang('upcoming_event') .'</h2>'.getPublicEventsForHome().
+								'<div class="cl">&nbsp;</div><br/><h2>'.lang('public_gallery').'</h2>'.$this->gallery->getGalleries(MEDIUM_LOAD_ITEMS,'event_public',getCart()).
+								'<div class="cl">&nbsp;</div>';
 		$this->load->view('header');
 		$this->load->view('home', isset($data) ? $data : NULL);
 		$this->load->view('footer');
--- application/views/home.php
+++ application/views/home.php
@@ -16,70 +16,7 @@
   <!-- welcome  -->
       <?php echo $page_info; ?>
       <!-- end welcome  -->
-  <!-- news -->
-      <div class="news">
-        
-        <!-- news item -->
-        <div class="news-item">
-         <span class="news-pointer-date"><b>10 Dec</b>&nbsp; <i>2012</i></span>
-          <div class="news-body">
-            <h3>Concert Voltaj</h3>
-            <a href="#" class="btn"><span><em>mai mult</em></span></a>
-            <div class="cl">&nbsp;</div>
-            <p>In data de 31.12.2012 Voltaj va concerta!!!<br>Cei care au ales sa faca Revelionul in Bucuresti vor putea sa ii vada in:
-<br>- Parcul Sebastian - ora 20:20,
-<br>- Piata Revolutiei - ora 22:45, 
-<br>- Piata Constitutiei - ora 00:15,
-<br>- Romexpo - ora 5:15.</p>
-          </div>
-          <div class="cl">&nbsp;</div>
-        </div>
-        <!-- end news item -->
-        
-        <!-- news item -->
-        <div class="news-item">
-        
-           <span class="news-pointer-date"><b>9 Nov</b>&nbsp; <i>2012</i></span>
-          <div class="news-body">
-            <h3>Ziua mondiala a fericirii</h3>
-            <a href="#" class="btn"><span><em>mai mult</em></span></a>
-            <div class="cl">&nbsp;</div>
-            <p>In curand incepe luna cadourilor alaturi de cei dragi. Pana atunci urmeaza insa petrecerea de Craciun cu colegii de servici. Iti doresti sa fie un moment de neuitat? Contacteaza-ne</p>
-          </div>
-          <div class="cl">&nbsp;</div>
-        </div>
-        <!-- end news item -->
-        
-        <!-- news item -->
-        <div class="news-item">
-          <span class="news-pointer-date"><b>29 Apr</b>&nbsp; <i>2011</i></span>
-          <div class="news-body">
-            <h3>Nunta la Palat</h3>
-            <a href="#" class="btn"><span><em>mai mult</em></span></a>
-            <div class="cl">&nbsp;</div>
-            <p>Pentru cei care isi doresc o nunta la palat, in Romania, variantele sunt multiple.</p>
-          </div>
-          <div class="cl">&nbsp;</div>
-        </div>
-        <!-- end news item -->
-        
-        <!-- news item -->
-        <div class="news-item">
-          <span class="news-pointer-date"><b>9 Nov</b>&nbsp; <i>2009</i></span>
-          <div class="news-body">
-            <h3>Deschiderea oficiala a sediului PropaDropa</h3>
-            <a href="#" class="btn"><span><em>mai mult</em></span></a>
-            <div class="cl">&nbsp;</div>
-            <p>Inaugurarea noului sediu a avut loc intr-o atmosfera de neuitat, cand noi am devenit proprii nostri clienti fideli in materie de organizat evenimente. pe harta de mai jos puteti localiza cu usurinta noul sediu. Va asteptam cu drag.</p>
-          </div>
-          <?php echo $gmap; ?>
-          <div class="cl">&nbsp;</div>
-        </div>
-        <!-- end news item -->
-        
-      </div>
-      <!-- end news -->
-
+  
 <!-- ============================================================= -->
 
   </div>
