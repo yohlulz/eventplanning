b84baa1bbe340d2384467a39a1813f2abb55d750
Ovidiu Maja <ovi.dan89@gmail.com>
2013-01-03 05:43:30 +0200

    Corrected order of b, u, i

--- application/language/de/general_lang.php
+++ application/language/de/general_lang.php
@@ -26,7 +26,7 @@ $lang['notice']									= 'Notificare';
 $lang['not_logged']								= 'Nu sunteti autentificat.<br> Logati-va pentru a putea continua.';
 $lang['contact_sidebar_message']				= 'Ne puteti contacta folosind formularul din coltul dreapta sus. Asteptam cu nerabdare sugestiile dumneavoastra.';
 $lang['welcome_message']						= '<h2>Bun venit</h2>
-											          <p>Scopul nostru este <b><u><i> un eveniment planificat:</b> </u></i>
+													<p>Scopul nostru este <b><u><i> un eveniment planificat:</i></u></b>
 											          <ul>
 											          <li> in detaliu la cateva click-uri distanta</li>
 											          <li> modelat dupa dorintele clientului </li>
